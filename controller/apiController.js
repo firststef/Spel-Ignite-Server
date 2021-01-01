c3864f9ba5664ac7e9d520750afceaf417cdbfa2
firststef <firstbossspike@gmail.com>
2021-01-02 00:12:33 +0200

    Added cors headers

--- controller/apiController.js
+++ controller/apiController.js
@@ -16,6 +16,8 @@ class ApiController {
     resolve(res, content, type){
         res.statusCode = 200;
         res.setHeader('Content-Type', type);
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.write(JSON.stringify(content));
         res.end();
     }
